cd92f0e024ac4575bf2bd2cf837ad885c2a4337a
Ryan Rehan <ryanrehan.pc@gmail.com>
2025-03-02 01:15:05 +0600

    finished button click feature --> alert after click the complete button, decreased the task assigned number, increase the task assigned number and when the task assigned number == to 0 then appear the last alert

--- script/tasks.js
+++ script/tasks.js
@@ -1,11 +1,38 @@
 // fetch the completed button
-
 const completedBtns = document.getElementsByClassName("complete-btn");
+
+// fetch the task assigned number
+let task = document.getElementById("task-assigned");
+let taskAssigned = document.getElementById("task-assigned").innerText;
+let taskAssignedNumber = parseInt(taskAssigned);
+
+// fetch the total task number
+let totalTask = document.getElementById("finished-task");
+let totalTaskValue = parseInt(document.getElementById("finished-task").innerText);
 
 for (const btn of completedBtns) {
     btn.addEventListener("click", () => {
+        alert("Board updated Successfully");
+        
+        // decrease the task number
+        taskAssignedNumber--;
+
+        // increase the total task number
+        totalTaskValue++;
+
+        // then update the task assigned number
+        task.innerText = taskAssignedNumber;
+
+        // then update the total task number
+        totalTask.innerText = totalTaskValue;
+
+        // condition for last number task finishing
+        if(taskAssignedNumber == 0) {
+            alert("Congrates!!! You have completed all the current task");
+        }
+
+        
         btn.classList.add("opacity-20");
-        console.log("yah");
         btn.disabled = true;
     })
 }
